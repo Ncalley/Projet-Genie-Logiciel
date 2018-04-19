0ac7d727783c1064185b9b514a66b4ba642b5af2
NCalley <nicolas.calley@laposte.net>
2018-04-19 10:44:16 +0200

    - Modifications NetworkObject    -> ajout des méthodes hashcode et equals - Ajout de la classe JSONProcessor.java

--- MetroProject/src/main/java/Metro/NetworkObject.java
+++ MetroProject/src/main/java/Metro/NetworkObject.java
@@ -1,5 +1,6 @@
 package Metro;
 
+
 /**
  * Abstract class of an object of the network, it will either be a Station or a Line.
  * Is Comparable, two objects with the same name are the same objects
@@ -10,12 +11,35 @@ package Metro;
  * @author Nicolas
  *
  */
-public abstract class NetworkObject implements Comparable{
+public abstract class NetworkObject implements Comparable<Object>{
 	private String name;
 	private boolean incident;
 	
 	//Methods
-	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NetworkObject other = (NetworkObject) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 	
 	
 	//Getters & Setters
